6429edb60e43828c77c13704b1c53063492b9505
Asel <asel.alimbaeva.kk@gmail.com>
2022-12-16 13:16:52 +0300

    feat: add pages

---
+++ react-redux-ts/src/pages/aboutProject/AboutProject.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const AboutProject: FC = () => {
+  return <div>AboutProject</div>;
+};
---
+++ react-redux-ts/src/pages/documentsPage/Documents.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from 'react';
+
+export const Documents: FC = () => {
+  return <div>Documents</div>;
+};
