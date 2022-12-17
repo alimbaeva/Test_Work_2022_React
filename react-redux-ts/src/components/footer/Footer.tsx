315e55bfc6d93d52f5616bce27aaa27a5baeaf5c
Asel <asel.alimbaeva.kk@gmail.com>
2022-12-17 09:11:27 +0300

    feat: add footer

--- react-redux-ts/src/components/footer/Footer.tsx
+++ react-redux-ts/src/components/footer/Footer.tsx
@@ -1,5 +1,29 @@
 import React, { FC } from 'react';
+import Frame from '../../assets/Frame.png';
+import image from '../../assets/image.png';
+import './footer.scss';
 
 export const Footer: FC = () => {
-  return <div className="container">Footer</div>;
+  return (
+    <footer>
+      <div className="container">
+        <div className="footer">
+          <div className="footer__title">
+            <img src={Frame} />
+            <p>Государственный Российский Дом народного творчества имени В.Д. Поленова</p>
+          </div>
+          <div className="footer__contact">
+            <p>Контакты</p>
+            <div>
+              <p>Москва, Сверчков пер., 8, стр. 3.</p>
+              <p>Тел.: +7 495 628-40-87</p>
+            </div>
+          </div>
+          <div>
+            <img src={image} />
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
