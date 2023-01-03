9cbc6dedf04f62eff265c043e67b844b03d71246
Asel <asel.alimbaeva.kk@gmail.com>
2023-01-03 10:32:05 +0300

    feat: add disabled button

--- react-redux-ts/src/components/cards/RenderCadrs.tsx
+++ react-redux-ts/src/components/cards/RenderCadrs.tsx
@@ -1,18 +1,56 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import { cardData } from '../../data/cardData';
 import { Card } from './Card';
+import { ICard } from '../../types/types';
+import { PaginationBtn } from '../paginationBtn/PaginationBtn';
 import './style/card.scss';
 
 export const RenderCadrs: FC = () => {
+  const [curentPage, setCurentPage] = useState('1');
+  const [cardsOnPage, setcardsOnPage] = useState(12);
+  const [cards, setCards] = useState<ICard[]>(cardData.slice(0, cardsOnPage));
+  const allPageNumbers = Math.ceil(cardData.length / cardsOnPage);
+
   const renderCards = () => {
-    return cardData.map((el) => {
+    return cards.map((el) => {
       return <Card key={el.id} props={el} />;
     });
+  };
+
+  const paginate = (
+    pageNumber: number,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    btnChilde: ChildNode | null | undefined
+  ) => {
+    let btn: HTMLButtonElement;
+    if (!!e) btn = e.target as HTMLButtonElement;
+    const buttons: ChildNode | NodeListOf<ChildNode> | undefined = btnChilde
+      ? btnChilde.childNodes
+      : btn!.parentElement?.childNodes;
+    if (!buttons) return;
+    buttons.forEach((el: ChildNode) => {
+      const btn = el as HTMLButtonElement;
+      btn.classList.remove('active');
+
+      if (btn.textContent === String(pageNumber + 1)) btn.classList.add('active');
+    });
+
+    const start = (Number(pageNumber + 1) - 1) * cardsOnPage;
+    const end = start + cardsOnPage;
+    setCurentPage(String(pageNumber + 1));
+    setCards(cardData.slice(start, end));
   };
 
   return (
     <div className="container">
       <div className="cards-block">{renderCards()}</div>
+      <div>
+        <PaginationBtn
+          allPageNumbers={allPageNumbers}
+          paginate={paginate}
+          curentPage={curentPage}
+        />
+      </div>
     </div>
   );
 };
---
+++ react-redux-ts/src/components/paginationBtn/PaginationBtn.tsx
@@ -0,0 +1,119 @@
+import { FC, useState } from 'react';
+import arrow_left from '../../assets/arrow_left.svg';
+import arrow_right from '../../assets/arrow_right.svg';
+
+interface Inum {
+  allPageNumbers: number;
+  paginate: (
+    pageNumber: number,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    btnChilde: ChildNode | null | undefined
+  ) => void;
+  curentPage: string;
+}
+
+export const PaginationBtn: FC<Inum> = ({ allPageNumbers, paginate, curentPage }: Inum) => {
+  const [disBtnLeft, setDisBtnLeft] = useState(true);
+  const [disBtnRight, setDisBtnRight] = useState(false);
+  const pages: number[] = [];
+
+  for (let i = 0; i < allPageNumbers; i++) {
+    pages.push(i);
+  }
+
+  const handlePagination = (
+    num: number,
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent> | null,
+    btnChilde: ChildNode | null | undefined
+  ) => {
+    paginate(num, e, btnChilde);
+  };
+
+  const handleClickRight = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const elem = e.target as HTMLButtonElement;
+    let previousSiblingEl: ChildNode | null | undefined = elem.previousSibling;
+    console.log(previousSiblingEl);
+    if (previousSiblingEl === null) {
+      previousSiblingEl = elem.parentNode?.previousSibling
+        ? elem.parentNode?.previousSibling
+        : elem.parentNode?.parentNode?.previousSibling;
+    }
+    if (!previousSiblingEl) return;
+
+    const leftBtn = previousSiblingEl.previousSibling as HTMLButtonElement;
+    if (Number(curentPage) === 1) {
+      leftBtn.disabled = false;
+      setDisBtnLeft(false);
+    }
+    if (Number(curentPage) === allPageNumbers - 1) {
+      elem.disabled = true;
+      setDisBtnRight(true);
+    }
+    handlePagination(Number(curentPage), null, previousSiblingEl);
+  };
+
+  const handleClickLeft = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    const elem = e.target as HTMLButtonElement;
+    let nextSiblingEl: ChildNode | null | undefined = elem.nextSibling;
+    console.log(nextSiblingEl);
+    if (nextSiblingEl === null) {
+      console.log(elem.parentNode?.nextSibling);
+      nextSiblingEl = elem.parentNode?.nextSibling
+        ? elem.parentNode?.nextSibling
+        : elem.parentNode?.parentNode?.nextSibling;
+    }
+    if (!nextSiblingEl) return;
+
+    const rightBtn = nextSiblingEl.nextSibling as HTMLButtonElement;
+    if (Number(curentPage) === allPageNumbers) {
+      rightBtn.disabled = false;
+      setDisBtnRight(false);
+    }
+
+    if (Number(curentPage) === 2) {
+      elem.disabled = true;
+      setDisBtnLeft(true);
+    }
+    handlePagination(Number(curentPage) - 2, null, nextSiblingEl);
+  };
+
+  return (
+    <div className="buttons">
+      <button className="arrow arrow-left" onClick={(e) => handleClickLeft(e)}>
+        {disBtnLeft && <button disabled={true} className="disabled-btn"></button>}
+        <svg width="7" height="11" viewBox="0 0 7 11" xmlns="http://www.w3.org/2000/svg">
+          <path d="M5.55505 0.727097L6.0267 1.07494L6.0267 10.5029L5.57189 10.8339L0.973282 6.09467L0.973282 5.39814L5.55505 0.727097Z" />
+        </svg>
+      </button>
+      <div className="pagination">
+        {pages.map((num: number) => {
+          if (num === 0) {
+            return (
+              <button key={num} className="active" onClick={(e) => handlePagination(num, e, null)}>
+                {num + 1}
+              </button>
+            );
+          } else {
+            return (
+              <button key={num} onClick={(e) => handlePagination(num, e, null)}>
+                {num + 1}
+              </button>
+            );
+          }
+        })}
+      </div>
+      <button className="arrow arrow-right" onClick={(e) => handleClickRight(e)}>
+        {disBtnRight && <button disabled={true} className="disabled-btn"></button>}
+        <svg
+          width="7"
+          height="11"
+          viewBox="0 0 7 11"
+          fill="current"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M1.44492 10.8339L0.973267 10.4861L0.973266 1.0581L1.42807 0.727097L6.02669 5.46636L6.02669 6.16289L1.44492 10.8339Z" />
+        </svg>
+      </button>
+    </div>
+  );
+};
